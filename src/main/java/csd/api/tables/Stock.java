69d045387195352e96d2478fc558216a2e7c1e0e
xue wenyu <wenyu.xue.2019@sis.smu.edu.sg>
2020-09-11 11:12:19 +0800

    Create Stock.java

---
+++ src/main/java/csd/api/tables/Stock.java
@@ -0,0 +1,32 @@
+package csd.api.tables;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.*;
+
+
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Stock {
+    private String symbol;
+    private double last_price;
+    private int bid_volume;
+    private double bidprice;
+    private int ask_volume;
+    private double askprice;
+
+
+}
