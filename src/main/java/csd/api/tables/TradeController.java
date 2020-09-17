b1dbd05adfe9108de47f1cabd28c9ea9c34646d5
xue wenyu <wenyu.xue.2019@sis.smu.edu.sg>
2020-09-17 16:34:40 +0800

    tradecontroller

---
+++ src/main/java/csd/api/tables/TradeController.java
@@ -0,0 +1,28 @@
+package csd.api.tables;
+
+import csd.api.tables.*;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TradeController {
+    private TradeRepository trade;
+
+    public TradeController(TradeRepository trade){
+        this.trade = trade;
+    }
+
+    
+}
