79a7df751b7766991afb3039420b56fae1b949e0
Thet Wai <twthet.2019@sis.smu.edu.sg>
2020-09-17 17:28:43 +0800

    Create Assest for Portfolio

---
+++ src/main/java/csd/api/tables/Assest.java
@@ -0,0 +1,35 @@
+package csd.api.tables;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne; //
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.*;
+
+
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Assest {
+    
+    // @Id @GeneratedValue (strategy = GenerationType.IDENTITY)
+    // private Long id;
+
+    private String code;
+    private int quantity;
+    private double avg_price;
+    private double current_price;
+    private double value;
+    private double gain_loss;
+    
+}
