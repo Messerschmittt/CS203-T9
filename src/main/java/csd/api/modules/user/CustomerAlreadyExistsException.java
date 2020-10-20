c91310465034700b8db59e12645bd0c27c0efc22
Ezekiel Young <eoyoung.2019@smu.edu.sg>
2020-10-20 23:23:13 +0800

    Minor Bug Fixes

--- src/main/java/csd/api/modules/user/CustomerAlreadyExistsException.java
+++ src/main/java/csd/api/modules/user/CustomerAlreadyExistsException.java
@@ -3,7 +3,7 @@ package csd.api.modules.user;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST) // 400 Error
+@ResponseStatus(HttpStatus.CONFLICT) // 400 Error
 public class CustomerAlreadyExistsException extends RuntimeException{
     /**
      *
